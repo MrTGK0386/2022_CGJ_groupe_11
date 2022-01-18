e66333b238c3c3fb6663afd9c04bf6619eee4f17
TheKingL <josukejoestar84@gmail.com>
2022-01-19 00:59:33 +0100

    Settings + Sound + Lvl1 [Beta]

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int coinsCount;
+    public Text coinsCountText;
+
+    public static Inventory instance;
+
     void Start()
     {
-        
+        coinsCountText.text = coinsCount.ToString();
+    }
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("+ d'un inventaire");
+            return;
+        }
+
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Addcoins(int count)
     {
-        
+        coinsCount+=count;
+        coinsCountText.text = coinsCount.ToString();
     }
 }
